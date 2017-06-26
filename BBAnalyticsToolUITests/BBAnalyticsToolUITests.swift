e627175541cb1b95051c1b16b7a9d0a59195efa6
Cory Alder <cory@buddybuild.com>
2017-06-26 11:00:28 -0700

    Add UITest for new relic

---
+++ BBAnalyticsToolUITests/BBAnalyticsToolUITests.swift
@@ -0,0 +1,43 @@
+//
+//  BBAnalyticsToolUITests.swift
+//  BBAnalyticsToolUITests
+//
+//  Created by Cory Alder on 2017-06-26.
+//  Copyright © 2017 Cory Alder. All rights reserved.
+//
+
+import XCTest
+
+class BBAnalyticsToolUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let masterNavigationBar = XCUIApplication().navigationBars["Master"]
+        // tap right bar button item (index 1) to try out using buddybuildViewIsPrivate with New Relic
+        masterNavigationBar.children(matching: .button).matching(identifier: "Add").element(boundBy: 1).tap()
+        
+        // tap left bar button item (index 0) to try out using buddybuild with aspects framework (haven't figured out how to make this fail, but let's do it anyway while we're here).
+        masterNavigationBar.children(matching: .button).matching(identifier: "Add").element(boundBy: 0).tap()
+    }
+    
+}
